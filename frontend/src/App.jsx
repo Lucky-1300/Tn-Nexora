7c597e766a8a59f6c084d98b2695cb99265c4652
AsAafiya <shaikhaafiyarasul2005@gmail.com>
2026-03-26 17:45:08 +0530

    added information pages of service cards

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import ContactPage from './components/Contact';
+import ServiceDetail from "./pages/ServiceDetail";
 import Blog from './pages/Blog';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/contact" element={<ContactPage />} />
       <Route path="/blog" element={<Blog />} />
+      <Route path="/services/:slug" element={<ServiceDetail />} />
     </Routes>
   );
 }
