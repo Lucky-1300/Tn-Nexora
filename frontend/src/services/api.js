aaf9f34b06fb0e2a39ace91b06d084d38d86114d
AsAafiya <shaikhaafiyarasul2005@gmail.com>
2026-04-07 21:20:08 +0530

    bug fix

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,25 +1,3 @@
-// import axios from "axios";
-
-// const api = axios.create({
-//   baseURL: "http://localhost:5000",
-// });
-
-// api.interceptors.response.use(
-//   (res) => res,
-//   (error) => {
-//     if (error.response?.status === 403) {
-//       window.location.href = "/403";
-//     }
-//     if (error.response?.status === 500) {
-//       window.location.href = "/500";
-//     }
-//     return Promise.reject(error);
-//   }
-// );
-
-// export default api;
-
-
 import axios from "axios";
 
 const api = axios.create({
