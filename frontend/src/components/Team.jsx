35f905f17269eb2a351d4fd8de7888e97f2b28f9
Utkarsh Kumar Jaiswal <13000123120@ticollege.org>
2026-04-08 23:33:53 +0530

    added pictures of team members

--- frontend/src/components/Team.jsx
+++ frontend/src/components/Team.jsx
@@ -20,7 +20,7 @@ const teamMembers = [
   {
     name: "Lucky Ray",
     role: "Full Stack Developer",
-    image: "/team/lucky.jpg",
+    image: "/Team/Lucky.jpeg",
     linkedin: "https://www.linkedin.com/in/lucky-ray-5a6642338?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
     desc: "Focuses on building efficient and modern web solutions with full-stack expertise.",
   },
